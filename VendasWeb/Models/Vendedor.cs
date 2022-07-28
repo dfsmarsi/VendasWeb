43fbbc921721e28fac1da6538a8d0cee2002c44b
dfsmarsi <douglassmarsi10@gmail.com>
2022-07-28 17:09:10 -0300

    trocando layout

---
+++ VendasWeb/Models/Vendedor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWeb.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
