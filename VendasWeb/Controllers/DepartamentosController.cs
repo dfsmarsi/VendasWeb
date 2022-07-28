6c30e8bd2ce83ed28479e59f43c02f73f452624e
dfsmarsi <douglassmarsi10@gmail.com>
2022-07-28 11:56:05 -0300

    criadas model view Controller Departamentos

---
+++ VendasWeb/Controllers/DepartamentosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWeb.Models;
+
+namespace VendasWeb.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> listDepartamentos = new List<Departamento>();
+            listDepartamentos.Add(new Departamento { Id = 1, Nome = "Eletronicos" });
+            listDepartamentos.Add(new Departamento { Id = 2, Nome = "Moda" });
+            return View(listDepartamentos);
+        }
+    }
+}
